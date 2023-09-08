62ac6486a67f64e2e72969c1083a377d08dbe8e2
Kamal1701 <kk17kannan@yahoo.com>
2023-09-08 14:36:40 +0530

    firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:oays/firebase_options.dart';
 import 'package:oays/screens/oays_signin_screen.dart';
 import 'package:oays/utils/themes/theme_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
